3eac728e148bbcf37bb08cbc3d6ae362ec503a16
Xudong Guo <guoxudong.dev@gmail.com>
2023-12-07 14:46:31 +0800

    update homepage

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: '热点信息知识库',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    Svg: require('@site/static/img/repository.svg').default,
     description: (
       <>
         收集国内外开源发展热点、政策走向、技术动态以及与数字经济等相关的发展态势
@@ -14,7 +14,7 @@ const FeatureList = [
   },
   {
     title: '行业一线实践专家供稿',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    Svg: require('@site/static/img/specialists.svg').default,
     description: (
       <>
         在横向研究方向上，扩展到开源发展战略、开源商业化、开源合规化、开源教育等专题研究以及开源生态白皮书等研究范畴
@@ -23,7 +23,7 @@ const FeatureList = [
   },
   {
     title: '做国内开源领域最具影响力的期刊',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    Svg: require('@site/static/img/journals.svg').default,
     description: (
       <>
         面向 1000 名开源利益相关方，外延影响数十万开源产业从业者的深度评论期刊
